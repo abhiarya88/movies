4547268fa8c2a60b3efaa1b67231d140d48dcd1e
abhiarya88 <harishnaik4488@gmail.com>
2025-02-02 20:11:35 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -115,7 +115,7 @@ User - {}"""
 ⭐ Rating: {rating} / 10</b>
 """
 
-    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇➥ 「<a href="https://t.me/kA15Rockers">𝗞𝗔𝟭𝟱𝗥𝗼𝗰𝗸𝗲𝗿𝘀</a>」</b>"""
+    FILE_CAPTION = """<b>{file_name}\n\n𝖩𝗈𝗂𝗇👉 「<a href="https://t.me/kA15Rockers">𝗞𝗔𝟭𝟱𝗥𝗼𝗰𝗸𝗲𝗿𝘀</a>」</b>"""
     
 
     ALRT_TXT = """ᴊᴀʟᴅɪ ʏᴇʜᴀ sᴇ ʜᴀᴛᴏ !"""
