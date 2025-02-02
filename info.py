d18c79c654f1454581999966f9c6ca89516b672f
abhiarya88 <harishnaik4488@gmail.com>
2025-02-02 11:10:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -46,14 +46,14 @@ IS_VERIFY = is_enabled('IS_VERIFY', True)
 #---------------------------------------------------------------
 TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
 VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'omegalinks.in')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'omegalinks.in')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'omegalinks.in')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
+SHORTENER_API = environ.get("SHORTENER_API", "639dc3a26d626c20a287aed9e3683c7a0a3cc160")
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'Shortxlinks.com')
+SHORTENER_API2 = environ.get("SHORTENER_API2", "f3de8cf18ee2173bb17d2c847714d2e3874ca5be")
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'ModiJiUrl.com')
+SHORTENER_API3 = environ.get("SHORTENER_API3", "cecb3ae504b6dd120115662bc839d16c")
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'Shortxlinks.com')
+TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "300"))
+THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "300"))
 #---------------------------------------------------------------
 #---------------------------------------------------------------
 LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
